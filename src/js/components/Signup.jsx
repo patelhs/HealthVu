9b861a642e0b145dc3d45be7a7e4fc44a450ea56
Hemal <phemals@gmail.com>
2015-11-19 09:16:09 -0500

    Registration Progress bar - Incomplete.

--- src/js/components/Signup.jsx
+++ src/js/components/Signup.jsx
@@ -1,16 +1,51 @@
-import React from 'react/addons';
+import React from 'react';
 import ReactMixin from 'react-mixin';
 import LoginActionCreators from '../actions/LoginActionCreators';
 
-export default class Signup extends React.Component {
+import AccountFields from './AccountFields';
+//import PracticeFields from './PracticeFields';
+//import BillingFields  from './BillingFields';
+//import Confirmation  from './Confirmation';
+//import Success  from './Success';
+//var SurveyFields  = require('./SurveyFields')
+import  assign from 'object-assign';
 
+
+var fieldValues = {
+    name: 'hemal',
+    email: null,
+    password: null,
+    practiceName: null,
+    practicePhone: null,
+    practiceWebsite: null,
+    practiceType: null,
+    practiceLocation: null,
+    practiceAddress: null,
+    billingContact: null,
+    billingAddress: null,
+    billingPhonbe: null,
+    cardNumber: null,
+    securityCode: null,
+    expirationDate: null
+  };
+
+export default class Signup extends React.Component {
   constructor() {
     super()
     this.state = {
       user: '',
       password: '',
-      extra: ''
+      extra: '',
+      step: 1
+      //fieldValues: fieldValues
+      
     };
+    this.onChange = this.nextStep.bind(this);
+
+ 
+ 
+  //this.state.step = 1;
+
   }
 
   signup(e) {
@@ -18,28 +53,78 @@ export default class Signup extends React.Component {
     LoginActionCreators.signup(this.state.user, this.state.password, this.state.extra)
   }
 
+
+  saveValues(field_value) {
+    alert("In save value");
+    return function() {
+      fieldValues = assign({}, fieldValues, field_value)
+    }.bind(this)();
+  }
+
+  nextStep() {
+    //this.setState({
+    //  step : this.state.step + 1
+    //})
+    var state = {user: '', password: '', extra: '', step: this.state.step+1};
+    this.setState(state);
+    alert(this.state.step);
+    //this.showStep();
+  }
+
+  previousStep() {
+    //this.setState({
+    //  step : this.state.step - 1
+    //})
+    this.state.step-=1;
+  }
+
+  submitRegistration() {
+    // Handle via ajax submitting the user data, upon
+    // success return this.nextStop(). If it fails,
+    // show the user the error but don't advance
+
+    this.nextStep();
+  }
+
+  showStep() {
+    switch (this.state.step) {
+      case 1:
+        return <AccountFields fieldValues={fieldValues}
+                              nextStep={this.nextStep.bind(this)}
+                              previousStep={this.previousStep.bind(this)}
+                              saveValues={this.saveValues.bind(this)} />
+      case 2:
+         return <AccountFields fieldValues={fieldValues}
+                              nextStep={this.nextStep.bind(this)}
+                              previousStep={this.previousStep.bind(this)}
+                              saveValues={this.saveValues.bind(this)} />
+      case 3:
+       return <BillingFields fieldValues={fieldValues}
+                             nextStep={this.nextStep}
+                             previousStep={this.previousStep}
+                             saveValues={this.saveValues} />
+      case 4:
+        return <Confirmation fieldValues={fieldValues}
+                             previousStep={this.previousStep}
+                             submitRegistration={this.submitRegistration} />
+      case 5:
+        return <Success fieldValues={fieldValues} />
+    }
+  }
+
   render() {
+     var style = {
+      width : (this.state.step / 4 * 100) + '%'
+    };
+
     return (
-      <div className="login jumbotron center-block">
-        <h1>Signup</h1>
-        <form role="form">
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input type="text" valueLink={this.linkState('user')} className="form-control" id="username" placeholder="Username" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" valueLink={this.linkState('password')} className="form-control" id="password" ref="password" placeholder="Password" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="extra">Extra</label>
-          <input type="text" valueLink={this.linkState('extra')} className="form-control" id="password" ref="password" placeholder="Some extra information" />
-        </div>
-        <button type="submit" className="btn btn-default" onClick={this.signup.bind(this)}>Submit</button>
-      </form>
-    </div>
+      <main>
+        <span className="progress-step">Step {this.state.step}</span>
+        <progress className="progress" style={style}></progress>
+        {this.showStep()}
+      </main>
     );
   }
 }
 
-ReactMixin(Signup.prototype, React.addons.LinkedStateMixin);
+//ReactMixin(Signup.prototype, React.addons.LinkedStateMixin);
