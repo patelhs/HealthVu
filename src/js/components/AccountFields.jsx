52898eebc19bdec155e06246920eab856c2d4222
Hemal <phemals@gmail.com>
2016-01-04 14:44:59 -0500

    divider between fields

--- src/js/components/AccountFields.jsx
+++ src/js/components/AccountFields.jsx
@@ -55,7 +55,7 @@ export default class AccountFields extends React.Component {
           name='name'
           label='Full Name'
           // You can pass params to validation rules
-          validate='required,isLength:3:60'
+          //validate='required,isLength:3:60'
           errorHelp={{
           required: 'Please enter your full name',
           isLength: 'Name must be at least 3 characters long'
@@ -71,7 +71,7 @@ export default class AccountFields extends React.Component {
           // Each input that you need validated should have
           // the "name" prop
           // Validation rules separated with comma
-          validate='required,isEmail'
+          //validate='required,isEmail'
           // Error messages for each error type
           errorHelp={{
           required: 'Please enter your email',
@@ -84,7 +84,7 @@ export default class AccountFields extends React.Component {
           name='password'
           label='Password'
           // You can pass params to validation rules
-          validate='required,isLength:6:60'
+          //validate='required,isLength:6:60'
           errorHelp={{
           required: 'Please specify a password',
           isLength: 'Password must be at least 6 characters'
@@ -96,7 +96,7 @@ export default class AccountFields extends React.Component {
           name='password-confirm'
           label='Confirm Password'
           // Validate can be a function as well
-          validate={(val, context) => val === context.password}
+          //validate={(val, context) => val === context.password}
           // If errorHelp property is a string, then it is used
           // for all possible validation errors
           errorHelp='Passwords do not match'
--- src/js/components/PracticeFields.jsx
+++ src/js/components/PracticeFields.jsx
@@ -4,12 +4,13 @@ import {Input, Button, ButtonInput} from 'react-bootstrap';
 import { Route, RouteHandler, Link } from 'react-router';
 import { Form, ValidatedInput } from 'react-bootstrap-validation';
 
-export default class PracticeFields extends React.Component {
+export default
+class PracticeFields extends React.Component {
   constructor(props) {
-        super(props);
-        this.state = {
-            name: props.fieldValues.name
-        };
+    super(props);
+    this.state = {
+      name: props.fieldValues.name
+    };
   }
 
   _handleValidSubmit(values) {
@@ -27,96 +28,134 @@ export default class PracticeFields extends React.Component {
 
   }
 
-  _loadForm(){
+  _handleBack(){
+    console.log("back button clicked");
+  }
+
+  _loadForm() {
     alert("loading");
   }
 
-   render() {
+  render() {
     return (
-      <Form
+      <div className="container">
+        <Form
+          onload={this._loadForm.bind(this)}
+          // Supply callbacks to both valid and invalid
+          // submit attempts
+          onValidSubmit={this._handleValidSubmit.bind(this)}
+          onInvalidSubmit={this._handleInvalidSubmit.bind(this)}>
 
-        onload={this._loadForm.bind(this)}
-        // Supply callbacks to both valid and invalid
-        // submit attempts
-        onValidSubmit={this._handleValidSubmit.bind(this)}
-        onInvalidSubmit={this._handleInvalidSubmit.bind(this)}>
+          <div className="row">
+            <div className='col-md-6'>
+              <div className="form-group">
+                <h2>Practice Details</h2>
+              </div>
+              <div className="form-group">
+                <ValidatedInput
+                  className="form-control"
+                  type='text'
+                  name='practiceName'
+                  label='Practice Name'
+                  // You can pass params to validation rules
+                  //validate='required,isLength:2:60'
+                  errorHelp={{
+                    required: 'Please enter the Practice Name',
+                    isLength: 'Name must be at least 2 characters long'
+                  }}
+                />
+              </div>
+              <div className="form-group">
+                <ValidatedInput
+                  type='number'
+                  name='practicePhone'
+                  label='Practice Phone'
+                  //className='input -primary'
+                  //placeholder="testing"
+                  // Each input that you need validated should have
+                  // the "name" prop
+                  // Validation rules separated with comma
+                  //validate='required,isNumeric,isLength:10:15' //will come back to this*
+                  // Error messages for each error type
+                  errorHelp={{
+                    required: 'Please enter the Practice Phone number',
+                    isNumeric: 'Number is invalid',
+                    isLength: 'Number is invalid'
+                  }}
+                />
+              </div>
+              <div className="form-group">
+                <ValidatedInput
+                  type='text'
+                  name='practiceWebsite'
+                  label='Practice Website'
+                  // You can pass params to validation rules
+                  //validate='required,isURL' // Need to come back to this**
+                  errorHelp={{
+                    required: 'Please enter Practice Website',
+                    isURL: 'Please enter a valid website'
+                  }}
+                />
+              </div>
+              <div className="form-group">
+                <ValidatedInput
+                  type="select"
+                  name="practiceType"
+                  label="Practice Type"
+                  placeholder="select"
+                  //validate="required"
+                  errorHelp="select is required">
+                  <option value="select">Medical</option>
+                  <option value="other">...</option>
+                </ValidatedInput>
+              </div>
+            </div>
+            <div className='col-md-6'>
+              <div className="form-group">
+                <h2>Practice Location</h2>
+              </div>
+              <div className="form-group">
+                <ValidatedInput
+                  type='text'
+                  name='practiceAddress'
+                  label='Practice Address'
+                  //validate='required,isLength:2:100'
+                  errorHelp={{
+                    required: 'Please enter Practice Address',
+                    isLength: 'Please enter a valid address'
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row">
 
-        <h2>Practice Details</h2>
-
-        <ValidatedInput
-          type='text'
-          name='practiceName'
-          label='Practice Name'
-          // You can pass params to validation rules
-          validate='required,isLength:2:60'
-          errorHelp={{
-          required: 'Please enter the Practice Name',
-          isLength: 'Name must be at least 2 characters long'
-        }}
-          />
-
-        <ValidatedInput
-          type='number'
-          name='practicePhone'
-          label='Practice Phone'
-          //className='input -primary'
-          //placeholder="testing"
-          // Each input that you need validated should have
-          // the "name" prop
-          // Validation rules separated with comma
-          validate='required,isNumeric,isLength:10:15' //will come back to this*
-          // Error messages for each error type
-          errorHelp={{
-          required: 'Please enter the Practice Phone number',
-          isNumeric: 'Number is invalid',
-          isLength: 'Number is invalid'
-        }}
-          />
-
-        <ValidatedInput
-          type='text'
-          name='practiceWebsite'
-          label='Practice Website'
-          // You can pass params to validation rules
-          validate='required,isURL' // Need to come back to this**
-          errorHelp={{
-          required: 'Please enter Practice Website',
-          isURL: 'Please enter a valid website'
-        }}
-          />
-
-        <ValidatedInput
-          type="select"
-          name="practiceType"
-          label="Practice Type"
-          placeholder="select"
-          validate="required"
-          errorHelp="select is required">
-          <option value="select">Medical</option>
-          <option value="other">...</option>
-        </ValidatedInput>
-
-        <h2>Practice Location</h2>
-
-        <ValidatedInput
-          type='text'
-          name='practiceAddress'
-          label='Practice Address'
-          validate='required,isLength:2:100'
-          errorHelp={{
-            required: 'Please enter Practice Address',
-            isLength: 'Please enter a valid address'
-          }}
-          />
-
-        <ButtonInput
-          type='submit'
-          bsSize='large'
-          bsStyle='primary'
-          value='Register'
-          />
-
-      </Form>
+            <div className='col-md-3'>
+              <div className="form-group">
+                <ButtonInput
+                  type='button'
+                  bsSize='large'
+                  bsStyle='primary'
+                  value='Back'
+                  onClick={this.props.previousStep.bind(this)}
+                />
+              </div>
+            </div>
+            <div className='col-md-3'>
+            </div>
+            <div className='col-md-6'>
+              <div className="form-group">
+                <ButtonInput
+                  type='submit'
+                  bsSize='large'
+                  bsStyle='primary'
+                  value='Continue'
+                />
+              </div>
+            </div>
+          </div>
+        </Form>
+      </div>
     )
   }
 }
